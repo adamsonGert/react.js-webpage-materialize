50928eddea5a24b17fa521216c26fda003ef4031
Gert Adamson <gert.adamson@aasaglobal.com>
2018-04-18 16:03:02 +0300

    Push stuff to repo

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+// components
+
+import Header from './components/headerComponent/header';
+import Footer from './components/footerComponent/footer';
+import Homepage from './components/pages/homePage';
+
+// include
+import './Assets/css/default.min.css';
+import './Assets/scripts/scripts.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+          <Homepage />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
